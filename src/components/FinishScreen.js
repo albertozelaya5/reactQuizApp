d0034f7e82b353d9636a8ffaaa21eee6f0274903
Alberto <albertolaniman@gmail.com>
2025-08-05 07:32:27 -0600

    Initial commit

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+
+export default function FinishScreen({ points, maxPossiblePoints, highScore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🏅";
+  if (percentage >= 80 && percentage < 100) emoji = "🥳";
+  if (percentage >= 50 && percentage < 80) emoji = "👍🏻";
+  if (percentage >= 0 && percentage < 50) emoji = "🤨";
+  if (percentage === 0) emoji = "☠️";
+
+  return (
+    <Fragment>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highScore} points)</p>
+
+      <button className="btn btn-ui" onClick={() => dispatch({ type: "restart" })}>
+        Restart quiz
+      </button>
+    </Fragment>
+  );
+}
